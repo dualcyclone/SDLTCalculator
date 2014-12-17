54fac8b7e181ef05f1ad0ff51ed15456cbfe93fe
Luke Dyson <luke.dyson@bskyb.com>
2014-12-17 08:04:57 +0000

    Adding social share tools

---
+++ js/social.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/*
+Social tool scripts
+ */
+
+// Twitter "Tweet" button:
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
+
+// Facebook "Like" button:
+(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&appId=739814742722005&version=v2.0";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
